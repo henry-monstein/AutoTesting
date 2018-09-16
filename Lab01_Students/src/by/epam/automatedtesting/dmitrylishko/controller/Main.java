2da223baf4fd0da8192430f0673a25cadbc8458d
Henry Monstein <1268620@gmail.com>
2018-09-16 19:06:30 +0300

    Stubents lab

---
+++ Lab01_Students/src/by/epam/automatedtesting/dmitrylishko/controller/Main.java
@@ -0,0 +1,16 @@
+package by.epam.automatedtesting.dmitrylishko.controller;
+
+import static by.epam.automatedtesting.dmitrylishko.model.logic.AverageMark.*;
+
+import by.epam.automatedtesting.dmitrylishko.model.entity.Group;
+import by.epam.automatedtesting.dmitrylishko.util.CreateGroup;
+import by.epam.automatedtesting.dmitrylishko.view.Read;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String fileName = "./resources/students.txt";
+        Group group = CreateGroup.create("group 2", Read.readFromFile(fileName));
+        System.out.println("Average mark of " + group.getName() + ": " + averageMark(group));
+    }
+}
